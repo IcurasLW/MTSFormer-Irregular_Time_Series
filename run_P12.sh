b525366aaccc716e4480198269700655d9215023
nathan <lwzheng777@outlook.com>
2024-07-22 15:48:08 +0930

    add script

---
+++ run_P12.sh
@@ -0,0 +1,7 @@
+for missing_ratio in "0" "0.1" "0.2"  "0.3"  "0.4"  "0.5" 
+do
+    CUDA_VISIBLE_DEVICES=0 python main.py \
+    --dataset P12 \
+    --missingratio $missing_ratio \
+    --feature_removal_level sample 
+done
